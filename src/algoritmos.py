6afdf89f46112e7781e8e1e0f40b129c35f2cbe6
Vitorlustosa <vgui435@gmail.com>
2026-06-20 14:54:59 -0300

    arrumando variaveis bfs

--- src/algoritmos.py
+++ src/algoritmos.py
@@ -1,22 +1,22 @@
-def bfs(grafo, palavra_inicial):
-    if not isinstance(grafo, dict) or not grafo:
+def bfs(graph, start_word):
+    if not isinstance(graph, dict) or not graph:
         raise ValueError("O grafo fornecido é inválido ou está vazio.")
     
-    if palavra_inicial not in grafo:
-        raise KeyError(f"A palavra '{palavra_inicial}' não existe no grafo.")
+    if start_word not in graph:
+        raise KeyError(f"A palavra '{start_word}' não existe no grafo.")
 
-    visitados = set()
-    fila = [palavra_inicial]
-    visitados.add(palavra_inicial)
-    resultado = []
+    visited = set()
+    queue = [start_word]
+    visited.add(start_word)
+    result = []
 
-    while fila:
-        vertice_atual = fila.pop(0)
-        resultado.append(vertice_atual)
+    while queue:
+        current_vertex = queue.pop(0)
+        result.append(current_vertex)
 
-        for vizinho in grafo.get(vertice_atual, {}):
-            if vizinho not in visitados:
-                visitados.add(vizinho)
-                fila.append(vizinho)
+        for neighbor in graph.get(current_vertex, {}):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
 
-    return resultado
+    return result
